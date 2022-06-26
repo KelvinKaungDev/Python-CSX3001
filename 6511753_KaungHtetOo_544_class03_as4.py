f532b7aa459c30ab202b97e99401322dc3d3cac8
Kaung Htet Oo <kaunghtetoo.dev@gmail.com>
2022-06-27 00:48:13 +0700

    Python Week 3 exercises

--- 6511753_KaungHtetOo_544_class03_as4.py
+++ 6511753_KaungHtetOo_544_class03_as4.py
@@ -5,8 +5,8 @@ third_digit = ( input_five_digits // 100 ) % 10
 fourth_digit = ( input_five_digits // 10 ) % 10
 fifth_digit = input_five_digits % 10
 
-swap_last_and_first_digit = fifth_digit * 10000 + second_digit * 1000 + third_digit * 100 + fourth_digit * 10 + first_digit
-swap_fourth_and_second_digit = first_digit * 10000 + fourth_digit * 1000 + third_digit * 100 + second_digit * 10 + fifth_digit
+swap_last_and_first_digit = fifth_digit * 10000 + second_digit * 1000 + third_digit * 100 + fourth_digit * 10 + first_digit;
+swap_fourth_and_second_digit = first_digit * 10000 + fourth_digit * 1000 + third_digit * 100 + second_digit * 10 + fifth_digit;
 
 if len(str(input_five_digits)) < 5 or len(str(input_five_digits)) > 5 :
     print('Wrong Input. Try Again.')
